8279e2f75201e7d2987f73f4d65f82abd814690f
Toby Scales <17342265+tobyscales@users.noreply.github.com>
2021-02-04 11:19:23 -0800

    Update Alias-Gcloud.ps1

--- functions/!required/Alias-Gcloud.ps1
+++ functions/!required/Alias-Gcloud.ps1
@@ -1,9 +1,12 @@
 function Run-gcloud {
-if ($args[0] -eq "c") {
-    if ($args[1] -eq "i") {
-    . gcloud compute instances $args[2-$args[-1]] } else {
-        . gcloud compute $args }
+    if ($args[0] -eq "c") {
+    $drop, $keep = $args #https://stackoverflow.com/questions/24754822/powershell-remove-item-0-from-an-array
+        if ($args[1] -eq "i") {
+            $drop, $keep = $keep
+            . gcloud compute instances $keep } else {
+        . gcloud compute $keep }
     . gcloud $args
+    }
 }
 
 Set-Alias g Run-gcloud
