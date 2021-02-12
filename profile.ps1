e3aebd633fd2d24cf309cd7445614407895818df
tobyscales <git@scales.cloud>
2021-02-11 16:13:11 -0800

    faster loadtime

--- profile.ps1
+++ profile.ps1
@@ -1,40 +1,17 @@
 if (-not $env:gitProfile) { $env:gitProfile = "tobyscales/ps-gitprofile" }
-$env:GIT_AUTHOR_NAME="tobyscales"
-$env:GIT_AUTHOR_EMAIL="vscode@github.com"
-
+if (-not $global:isConnected) { if ($PSVersionTable.PSVersion.Major -ge 6) { $global:isConnected = (test-connection "windows.net" -TCPPort 80 -quiet) } else { $global:isConnected = (Test-Connection 1.1.1.1 -count 1 -Quiet) } }
 function Update-GitProfile {
     param([Parameter(ValueFromPipeline = $true)]
         [String[]]$gitProfile = $env:gitProfile)
 
-    $getGitURL     = "https://raw.githubusercontent.com/$gitProfile/master/functions/!required/Get-GitProfile.ps1"
-    $gitProfileURL = "https://raw.githubusercontent.com/$gitProfile/master/Git.PowerShell_profile.ps1"
-
-    #TODO: use runspaces for faster loading?
-    #$runspaceURL = "https://raw.githubusercontent.com/pldmgg/misc-powershell/master/MyFunctions/PowerShellCore_Compatible/New-Runspace.ps1"
-    #$runspaceURL = "https://raw.githubusercontent.com/RamblingCookieMonster/Invoke-Parallel/master/Invoke-Parallel/Invoke-Parallel.ps1"
-    #Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($runspaceURL)) 
-
-    #$ErrorActionPreference = 'SilentlyContinue'
-    if (-not $global:isConnected) {
-        if ($PSVersionTable.PSVersion.Major -ge 6) { $global:isConnected = (test-connection "windows.net" -TCPPort 80 -quiet) } else { $global:isConnected = (Test-Connection 1.1.1.1 -count 1 -Quiet) }
-    }
+    $getGitURL = "https://raw.githubusercontent.com/$gitProfile/master/functions/!required/Get-GitProfile.ps1"
     
     if ($global:isConnected) { 
+        Write-host -ForegroundColor Green "Running in online mode... "
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($getGitURL))
-
-        if (test-path $home\.gitprofile\secrets.ps1) {
-            & "$home\.gitprofile\secrets.ps1"
-            Write-host -ForegroundColor Green "Running in online mode."
-            Get-GitProfile $gitProfileURL > $env:LocalGitProfile
-            return [scriptblock]::Create(
-                [io.file]::ReadAllText($env:LocalGitProfile)
-            )
-        }
-        else {
-            return [scriptblock]::Create(
-                (Get-GitProfile "https://raw.githubusercontent.com/$env:gitProfile/master/Git.PowerShell_profile.ps1").tostring()
-            )
-        }
+        return [scriptblock]::Create(
+            (Get-GitProfile "https://raw.githubusercontent.com/$env:gitProfile/master/Git.PowerShell_profile.ps1").tostring()
+        )
     }
     else {
         Write-Host -foregroundcolor yellow "Running in offline mode."
