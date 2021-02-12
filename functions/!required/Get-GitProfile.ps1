b110f788bfd2b9f25e4332f5c1cba9ea556f79f2
tobyscales <git@scales.cloud>
2021-02-12 09:32:44 -0800

    add try catch to update-profile

--- functions/!required/Get-GitProfile.ps1
+++ functions/!required/Get-GitProfile.ps1
@@ -1,19 +1,19 @@
 function global:Set-GitProfile {
     param([Parameter( Mandatory, ValueFromPipeline = $true)]
-        [String[]]$gitProfileURL)
+        $gitProfileURL)
         
     if (-not (test-path $profile)) { New-Item -ItemType File -Path $profile -Force | Out-Null } 
     Get-GitProfile $gitProfileURL > $profile
 }
 function global:Get-GitProfile {
-    param([Parameter( ValueFromPipeline = $true)]
+    param([Parameter( Mandatory, ValueFromPipeline = $true)]
         $gitProfileURL)
     
     return (New-Object System.Net.WebClient).DownloadString($gitProfileURL)
 }
 function global:Import-RequiredFunctions {
-    param([Parameter( ValueFromPipeline = $true)]
-        [String[]]$gitProfile)
+    param([Parameter( Mandatory, ValueFromPipeline = $true)]
+        $gitProfile)
 
     # Load all !required functions
     $wr = Invoke-WebRequest -Uri "https://api.github.com/repos/$gitProfile/contents/functions/!required"
--- profile.ps1
+++ profile.ps1
@@ -1,19 +1,19 @@
 if (-not $env:gitProfile) { $env:gitProfile = "tobyscales/ps-gitprofile" }
-if (-not $global:isConnected) { if ($PSVersionTable.PSVersion.Major -ge 6) { $global:isConnected = (test-connection "1.1.1.1" -TCPPort 53 -quiet) } else { $global:isConnected = (Test-Connection 1.1.1.1 -count 1 -Quiet) } }
+#if (-not $global:isConnected) { if ($PSVersionTable.PSVersion.Major -ge 6) { $global:isConnected = (test-connection "1.1.1.1" -TCPPort 53 -quiet) } else { $global:isConnected = (Test-Connection 1.1.1.1 -count 1 -Quiet) } }
 function Update-GitProfile {
     param([Parameter(ValueFromPipeline = $true)]
         [String[]]$gitProfile = $env:gitProfile)
 
-    if ($global:isConnected) { 
+    #  if ($global:isConnected) { 
+        
+    try {
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/tobyscales/ps-gitprofile/master/functions/!required/Get-GitProfile.ps1")) -ErrorAction Continue
+        $global:isConnected = $true
         Write-host -ForegroundColor Green "Running in online mode."
-        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/tobyscales/ps-gitprofile/master/functions/!required/Get-GitProfile.ps1"))
-        return [scriptblock]::Create(
-            (Get-GitProfile "https://raw.githubusercontent.com/$gitProfile/master/Git.PowerShell_profile.ps1").tostring()
-        )
     }
-    else {
+    catch { 
+        $global:isConnected = $false
         Write-Host -ForegroundColor Yellow "Running in offline mode."
-
         if (-not $env:LocalGitProfile) {
             return [scriptblock]::Create("Must be connected to run setup.") 
         }
@@ -23,6 +23,14 @@ function Update-GitProfile {
             )
         }    
     }
+    return [scriptblock]::Create(
+        (Get-GitProfile "https://raw.githubusercontent.com/$gitProfile/master/Git.PowerShell_profile.ps1").tostring()
+    )
+    # }
+    #else {
+
+       
+    #}
 
 }
 
