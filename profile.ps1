8a9ba89fb9589fc9ab36c48e0065ac898c05b440
tobyscales <git@scales.cloud>
2021-02-12 14:35:40 -0800

    yeeeesh

--- profile.ps1
+++ profile.ps1
@@ -59,19 +59,28 @@ function global:Import-RequiredFunctions {
 }
 function global:Import-GitFunction {
     Param(
+        [string]$functionName
+    )
+    . ( Import-GF $functionName)
+}
+function Import-GF {
+    Param(
         [string]$functionName,
         [string]$gitProfile = $env:gitProfile
     )
     if (-not $functionName.endswith(".ps1")) { $functionName += ".ps1" }
     $wr = Invoke-WebRequest -Uri "https://api.github.com/repos/$gitProfile/contents/functions"
-    
+
     $objects = $wr.Content | ConvertFrom-Json
     $url = $objects | where-object { $_.type -eq "file" -and $_.name.toUpper() -eq $functionName.toUpper() } | Select-object -exp download_url
-    
+
     write-host "Importing $functionName from $url"
-    invoke-expression ((New-Object System.Net.WebClient).DownloadString($url)) -ErrorAction Stop
-    #$sb = [scriptblock]::Create((New-Object System.Net.WebClient).DownloadString($url))
-    #$sb | iex
+    #invoke-expression ((New-Object System.Net.WebClient).DownloadString($url)) -ErrorAction Stop
+    #$fDef = ((New-Object System.Net.WebClient).DownloadString($url))
+    $funcName = ($functionname.split('.')[0])
+    #New-Item -Path Function:$funcName -Value $fDef -options AllScope
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($url))
+    #New-Item -Path Function:$funcName -Value $fDef -options AllScope
 }
 # function Get-GitProfile {
 #     param([Parameter( Mandatory, ValueFromPipeline = $true)]
