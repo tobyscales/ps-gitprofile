0f6a588910cdebfcb3cf5db1cc2930d608f45a1a
tobyscales <git@scales.cloud>
2021-02-12 09:39:04 -0800

    add try catch to update-profile

--- profile.ps1
+++ profile.ps1
@@ -1,15 +1,15 @@
 if (-not $env:gitProfile) { $env:gitProfile = "tobyscales/ps-gitprofile" }
-#if (-not $global:isConnected) { if ($PSVersionTable.PSVersion.Major -ge 6) { $global:isConnected = (test-connection "1.1.1.1" -TCPPort 53 -quiet) } else { $global:isConnected = (Test-Connection 1.1.1.1 -count 1 -Quiet) } }
 function Update-GitProfile {
     param([Parameter(ValueFromPipeline = $true)]
         [String[]]$gitProfile = $env:gitProfile)
 
-    #  if ($global:isConnected) { 
-        
     try {
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/tobyscales/ps-gitprofile/master/functions/!required/Get-GitProfile.ps1")) -ErrorAction Continue
         $global:isConnected = $true
         Write-host -ForegroundColor Green "Running in online mode."
+        return [scriptblock]::Create(
+            (Get-GitProfile "https://raw.githubusercontent.com/$gitProfile/master/Git.PowerShell_profile.ps1").tostring()
+        )    
     }
     catch { 
         $global:isConnected = $false
@@ -23,15 +23,6 @@ function Update-GitProfile {
             )
         }    
     }
-    return [scriptblock]::Create(
-        (Get-GitProfile "https://raw.githubusercontent.com/$gitProfile/master/Git.PowerShell_profile.ps1").tostring()
-    )
-    # }
-    #else {
-
-       
-    #}
-
 }
 
 . ( Update-GitProfile )
