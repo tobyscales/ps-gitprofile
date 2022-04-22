dd6739554d7c99dc139e17fd5b6718b4bd8048f5
Toby Scales <17342265+tobyscales@users.noreply.github.com>
2022-04-22 11:36:45 -0600

    add TF aliases

--- functions/!required/Alias-Commands.ps1
+++ functions/!required/Alias-Commands.ps1
@@ -5,10 +5,22 @@ function Run-kubedescribe {
     . kubectl describe $args
 }
 
-function Run-kubedelete {
+function Run-KubeDelete {
     . kubectl delete $args
 }
 
+function Run-TerraformPlan {
+    . terraform plan $args
+}
+
+function Run-TerraformApply {
+    . terraform apply -auto-approve $args
+}
+
+
 Set-Alias k Run-kubectl
 set-alias kd Run-kubedescribe
-set-alias kdel Run-kubedelete
+set-alias kdel Run-KubeDelete
+
+Set-Alias tfp Run-TerraformPlan
+Set-Alias tfa Run-TerraformApply
